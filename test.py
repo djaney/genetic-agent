53e220eeca6e1118f226d3fb708eb81257434e1d
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-04-13 15:57:23 +0800

    updates

--- test.py
+++ test.py
@@ -3,16 +3,16 @@ import numpy as np
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(('', 8888))
 
-for _ in range(1000):
+while True:
     scores = []
     for i in range(10):
         inp = 1
         s.send('act {} {}'.format(i, inp).encode())
         res = s.recv(1024)
         res = abs(float(res.decode('utf-8')))
-        score = res - inp+1
+        score = 10 - res - inp+1
         s.send('rec {} {}'.format(i, score).encode())
         scores.append(score)
     print(np.mean(scores))
-
-
+    if np.mean(scores) == 10:
+        break
