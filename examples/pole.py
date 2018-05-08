c83f8ffd6c042e5f0caee1c7cdba31014cca65c9
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-05-08 09:52:05 +0800

    snapshot

---
+++ examples/pole.py
@@ -0,0 +1,46 @@
+import gym
+
+import numpy as np
+from evolution import Species
+
+env = gym.make('CartPole-v1')
+
+done = False
+
+strain_count = 10
+
+agent = Species(input_count=env.observation_space.shape[0], output_count=env.action_space.n, hidden=1, depth=1,
+                strain_count=strain_count)
+
+# learn
+gen = 0
+while True:
+
+    scores = []
+    max_score = 0
+
+    for i in range(strain_count):
+        reward_sum = 0
+        ob = env.reset()
+        while True:
+            action = agent.act(ob, i)
+            ob, reward, done, info = env.step(np.argmax(action))
+            reward_sum = reward_sum + reward
+            # env.render()
+            if done:
+                break
+        agent.record(reward_sum, i)
+        scores.append(reward_sum)
+    print("generation {} max score {}".format(agent.current_generation, np.max(scores)))
+
+    if np.max(scores) >= 500:
+        break
+    else:
+        agent.evolve()
+
+ob = env.reset()
+while True:
+    action = agent.act(ob, 0)
+    ob, reward, done, info = env.step(np.argmax(action))
+    scores.append(reward_sum)
+    env.render()
