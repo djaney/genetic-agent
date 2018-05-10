456dc7d59d0cb0e9961c6fe1968457138e0a95b5
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-05-10 13:17:13 +0800

    snapshot

--- examples/mountaincar-v0.py
+++ examples/mountaincar-v0.py
@@ -8,7 +8,7 @@ env = gym.make('MountainCarContinuous-v0')
 done = False
 
 strain_count = 10
-passing_score = 1
+generation_check = 10
 agent = Species(input_count=env.observation_space.shape[0], output_count=env.action_space.shape[0], hidden=2, depth=1,
                 strain_count=strain_count, final_activation='tanh')
 
@@ -25,7 +25,7 @@ while True:
         while True:
             action = agent.act(ob, i)
             ob, reward, done, info = env.step(action)
-            reward_sum = reward_sum + reward
+            reward_sum = np.max([reward_sum, reward])
             # env.render()
             if done:
                 break
@@ -33,17 +33,18 @@ while True:
         scores.append(reward_sum)
     print("generation {} max score {}".format(agent.current_generation, np.max(scores)))
 
-    if np.max(scores) >= passing_score:
-        break
+    if 0 == agent.current_generation % generation_check:
+        agent.evolve()
+        ob = env.reset()
+        while True:
+            action = agent.act(ob, 0)
+            ob, reward, done, info = env.step(action)
+            scores.append(reward_sum)
+            env.render()
+            if done:
+                break
     else:
         agent.evolve()
 
-ob = env.reset()
-while True:
-    action = agent.act(ob, 0)
-    ob, reward, done, info = env.step(action)
-    scores.append(reward_sum)
-    env.render()
-    if done:
-        ob = env.reset()
+
 
