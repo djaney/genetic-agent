a248dd99dec46f0461aa07aa3902bd6dd9162127
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-05-10 22:02:14 +0800

    snapshot

--- examples/mountaincar-v0.py
+++ examples/mountaincar-v0.py
@@ -14,43 +14,44 @@ agent = Species(input_count=2, output_count=3, hidden=3, depth=0,
 
 agent.strains[0].summary()
 
+
+def play(a, e, index, render=False):
+    reward_sum = 200
+    max_position = -1.2
+    min_position = 0.6
+    ob = e.reset()
+    while True:
+        action = a.act(ob, index)
+        ob, reward, done, info = e.step(np.argmax(action))
+        max_position = np.max([max_position, ob[0]])
+        min_position = np.min([min_position, ob[0]])
+        reward_sum = reward_sum + reward
+        if render:
+            env.render()
+
+        if done:
+            break
+
+    # negative_reward + distance covered
+    reward_sum = reward_sum + ((max_position + 1.2) - (min_position + 1.2))
+
+    return reward_sum
+
+
 # learn
-gen = 0
 while True:
 
     scores = []
     max_score = 0
 
+    # play every strain
     for i in range(strain_count):
-        reward_sum = 200
-        max_position = -1.2
-        min_position = 0.6
-        ob = env.reset()
-        while True:
-            action = agent.act(ob, i)
-            ob, reward, done, info = env.step(np.argmax(action))
-            max_position = np.max([max_position, ob[0]])
-            min_position = np.min([min_position, ob[0]])
-            reward_sum = reward_sum + reward
-            # env.render()
-            if done:
-                break
-        # negative_reward + distance covered
-        reward_sum = reward_sum + ((max_position + 1.2) - (min_position + 1.2))
-        agent.record(reward_sum, i)
-        scores.append(reward_sum)
+        score = play(agent, env, i)
+        agent.record(score, i)
+        scores.append(score)
     print("generation {} max score {}".format(agent.current_generation, np.max(scores)))
 
     agent.evolve()
 
     if 0 == agent.current_generation % generation_check:
-        ob = env.reset()
-        while True:
-            action = agent.act(ob, 0)
-            ob, reward, done, info = env.step(np.argmax(action))
-            env.render()
-            if done:
-                break
-
-
-
+        play(agent, env, 0, True)
