9b4e998bcb1878893cc84c0f13bbe8bb0db08436
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-05-11 20:21:54 +0800

    snapshot

--- neat.py
+++ neat.py
@@ -1,7 +1,18 @@
 class Genome:
-    def __init__(self):
+    def __init__(self, input_count, output_count):
         self.nodes = []
         self.connections = []
+        self.id_counter = 1
+
+        for _ in range(input_count):
+            self.create_node(Node.TYPE_INPUT)
+
+        for _ in range(input_count):
+            self.create_node(Node.TYPE_OUTPUT)
+
+    def create_node(self, node_type):
+        self.nodes.append(Node(self.id_counter, node_type))
+        self.id_counter = self.id_counter + 1
 
     def mutate_nodes(self):
         pass
@@ -15,11 +26,18 @@ class Node:
     TYPE_OUTPUT = 2
     TYPE_HIDDEN = 3
 
-    def __init__(self, name, type):
-        self.name = name
-        self.type = type
+    def __init__(self, number, node_type):
+        self.number = number
+        self.node_type = node_type
         self.in_connections = []
         self.out_connections = []
+
+    def get_id(self):
+        return self.number
+
+    def get_type(self):
+        return self.node_type
+
 
     def connect_to(self, next_node):
         # create connection
@@ -29,11 +47,23 @@ class Node:
         # attach to next
         next_node.in_connections.append(connection)
         # set connection codes
-        connection.node_in = self
-        connection.node_out = next_node
+        connection.in_node = self
+        connection.out_node = next_node
+
+    def get_next_connections(self):
+        return self.out_connections
+
+    def get_prev_connections(self):
+        return self.in_connections
 
 
 class Connections:
     def __init__(self):
-        self.node_in = None
-        self.node_out = None
+        self.in_node = None
+        self.out_node = None
+
+    def get_next(self):
+        return self.out_node
+
+    def get_prev(self):
+        return self.in_node
--- neat_test.py
+++ neat_test.py
@@ -1,10 +1,47 @@
 import unittest
 from neat import Node
+from neat import Genome
+
 
 class TestNodeMethods(unittest.TestCase):
 
     def test_connect_to(self):
-        pass
+        n1 = Node(1, Node.TYPE_INPUT)
+        n2 = Node(2, Node.TYPE_OUTPUT)
+        n1.connect_to(n2)
+
+        self.assertEqual(2, n1.get_next_connections()[0].out_node.number)
+
+
+class TestConnectionMethods(unittest.TestCase):
+
+    def test_next(self):
+        n1 = Node(1, Node.TYPE_INPUT)
+        n2 = Node(2, Node.TYPE_OUTPUT)
+        n1.connect_to(n2)
+
+        self.assertEqual(2, n1.get_next_connections()[0].get_next().number)
+
+    def test_prev(self):
+        n1 = Node(1, Node.TYPE_INPUT)
+        n2 = Node(2, Node.TYPE_OUTPUT)
+        n1.connect_to(n2)
+
+        self.assertEqual(1, n2.get_prev_connections()[0].get_prev().number)
+
+
+class TestGenomeMethods(unittest.TestCase):
+
+    def test_create_node(self):
+        g = Genome(2, 1)
+        self.assertEqual(1, g.nodes[0].get_id())
+        self.assertEqual(2, g.nodes[1].get_id())
+        self.assertEqual(3, g.nodes[2].get_id())
+
+        self.assertEqual(Node.TYPE_INPUT, g.nodes[0].get_type())
+        self.assertEqual(Node.TYPE_INPUT, g.nodes[1].get_type())
+        self.assertEqual(Node.TYPE_OUTPUT, g.nodes[2].get_type())
+
 
 if __name__ == '__main__':
     unittest.main()
